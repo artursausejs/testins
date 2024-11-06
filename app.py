e9bd7923a98ce41b8aaacb5ac0972a9aa485175a
Glitch (marble-seemly-pond) <none>
2024-11-06 12:55:15 +0000

    komentars

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World!</p><img src='https://cdn.glitch.com/a9975ea6-8949-4bab-addb-8a95021dc2da%2Fillustration.svg?v=1618177344016' width='200px'>" 
   
 if __name__ == '__main__':
     app.run()
